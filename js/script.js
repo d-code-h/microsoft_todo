d6cc63ddaa6a23daf48909ceccd02e7a1d2fad1f
Habeeb David <habeebdh1@outlook.com>
2022-06-17 06:06:16 +0000

    Make script load after full document load

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-document.getElementById("completed-header").addEventListener("click", function(){
+function submit(){
+  alert("Sent")
+}
+
+window.addEventListener("load", function(){
+  document.getElementById("completed-header").addEventListener("click", function(){
   let content = document.getElementById("completed-content").style.display;
   if (content === "" || content === "none"){
     document.querySelector('i.fa-angle-right').setAttribute("class", "fa fa-angle-down");
@@ -9,15 +14,17 @@ document.getElementById("completed-header").addEventListener("click", function()
   }
 });
 
-document.querySelector("span .fa-plus").addEventListener("click", function(){
+  document.querySelector("span .fa-plus").addEventListener("click", function(){
   document.getElementById("form").style.display = "block";
   this.style.display = "none";
 })
 
-document.querySelector('input[name="add"]').addEventListener("blur", function(){
+  document.querySelector('input[name="add"]').addEventListener("blur", function(){
   document.querySelector("span  .fa-plus").style.display = "flex";
   document.getElementById("form").style.display = "none";
 })
-window.addEventListener("resize", (e) => {
+
+  window.addEventListener("resize", (e) => {
         console.log(e.target);
       });
+})  
